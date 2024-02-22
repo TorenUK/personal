dd89ee13677c3c99b2967f6f9c19387f8d22fcc8
TorenUK <toren@toren.uk>
2024-02-22 10:35:32 +0000

    feat: add local storage context

---
+++ src/context/localStorage.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useState, useEffect } from "react";
+
+// local storage data
+interface LocalStorageData {
+  name: string;
+}
+
+// context type
+export interface LocalStorageContextType {
+  data: LocalStorageData;
+  setData: React.Dispatch<React.SetStateAction<LocalStorageData>>;
+  getData: () => LocalStorageData
+}
+
+// create context
+const LocalStorageContext = createContext<LocalStorageContextType | undefined>(
+  undefined
+);
+
+// local storage provider props
+interface LocalStorageProviderProps {
+  children: React.ReactNode;
+}
+
+export const LocalStorageProvider: React.FC<LocalStorageProviderProps> = ({
+  children,
+}) => {
+  // retrieve data from local storage on initial render
+  const storedData: LocalStorageData = JSON.parse(
+    localStorage.getItem("appData") || '{"name": ""'
+  );
+  const [data, setData] = useState<LocalStorageData>(storedData);
+
+  useEffect(() => {
+    localStorage.setItem('appData', JSON.stringify(data))
+  }, [data])
+
+  const getData = (): LocalStorageData => data
+
+
+  return (
+    <LocalStorageContext.Provider value={{data, setData, getData}}>
+        {children}
+    </LocalStorageContext.Provider>
+  )
+
+};
+
+export default LocalStorageContext
